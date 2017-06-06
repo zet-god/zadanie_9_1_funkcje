38b7ed830d260937ee1b1b27003b9d7b23aabb94
Zuzanna Godek <[omitted]>
2017-06-06 16:11:28 +0200

    utworzono funkcję getTriangleArea

---
+++ scripts.js
@@ -0,0 +1,14 @@
+//alert('hello');
+
+function getTriangleArea(a, h) {
+	if (a >= 0 && h >= 0) {
+		return(a*h/2);
+	}
+	else {
+		console.log('Nieprawidłowe dane');
+	}
+}
+
+var triangleArea1 = getTriangleArea(10, 15);
+var triangleArea2 = getTriangleArea(8, 18);
+var triangleArea3 = getTriangleArea(2.5, 6.8);
