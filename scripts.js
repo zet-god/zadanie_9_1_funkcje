8c199415a64691e8920afbade7efd01fb369f7b8
Zuzanna Godek <[omitted]>
2017-06-16 11:52:03 +0200

    poprawki w pliku scripts.js

--- scripts.js
+++ scripts.js
@@ -1,15 +1,16 @@
 //alert('hello');
 
 function getTriangleArea(a, h) {
-	if (a >= 0 && h >= 0) {
+	if (a > 0 && h > 0) {
 		return a * h / 2;
 	}
 	else {
-		console.log('Nieprawidłowe dane');
+		return 'Nieprawidłowe dane';
 	}
 }
 
 var triangleArea0 = getTriangleArea(-10, 2);
+console.log(triangleArea0);
 
 var triangleArea1 = getTriangleArea(10, 15);
 var triangleArea2 = getTriangleArea(8, 18);
